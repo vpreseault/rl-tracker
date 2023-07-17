8806c98d135c72d67b655cf2ecbc3a0738f5194d
unknown <vincent.preseault@gmail.com>
2023-07-17 18:35:44 -0400

    adding multi gamemode support

--- Store.py
+++ Store.py
@@ -4,11 +4,13 @@ from Utility import *
 
 
 class Store:
-    def __init__(self):
+    def __init__(self, gamemode):
         file = open("data.json", "r")
         data = json.load(file)
         file.close()
         self.data = data
+
+        self.gamemode = gamemode
 
     def write(self):
         file = open("data.json", "w")
@@ -16,24 +18,24 @@ class Store:
         file.close()
 
     def addSession(self, sessionResult):
-        self.data["s14"]["sessions"].append(sessionResult)
+        self.data["s14"][self.gamemode]["sessions"].append(sessionResult)
         self.updateCurrentMmr(sessionResult["endMmr"])
         self.updateWinsLosses(sessionResult["wins"], sessionResult["losses"])
         self.write()
 
     def updateCurrentMmr(self, newMmr):
-        self.data["s14"]["regularSeason"]["currentMmr"] = newMmr
+        self.data["s14"][self.gamemode]["regularSeason"]["currentMmr"] = newMmr
 
     def updateWinsLosses(self, wins, losses):
-        self.data["s14"]["regularSeason"]["wins"] += wins
-        self.data["s14"]["regularSeason"]["losses"] += losses
-        self.data["s14"]["regularSeason"]["winrate"] = calculateWinrate(
-            self.data["s14"]["regularSeason"]["wins"],
+        self.data["s14"][self.gamemode]["regularSeason"]["wins"] += wins
+        self.data["s14"][self.gamemode]["regularSeason"]["losses"] += losses
+        self.data["s14"][self.gamemode]["regularSeason"]["winrate"] = calculateWinrate(
+            self.data["s14"][self.gamemode]["regularSeason"]["wins"],
             (
-                self.data["s14"]["regularSeason"]["wins"]
-                + self.data["s14"]["regularSeason"]["losses"]
+                self.data["s14"][self.gamemode]["regularSeason"]["wins"]
+                + self.data["s14"][self.gamemode]["regularSeason"]["losses"]
             ),
         )
 
     def getCurrentMmr(self):
-        return self.data["s14"]["regularSeason"]["currentMmr"]
+        return self.data["s14"][self.gamemode]["regularSeason"]["currentMmr"]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Session import Session
 from Store import Store
 
-store = Store()
+store = Store(gamemode="doubles")
 s = Session(store.getCurrentMmr())
 sessionResult = s.loop()
 store.addSession(sessionResult)
